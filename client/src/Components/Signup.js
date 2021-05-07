2da2202159abd49b6b6c6ac72ac53589bd3cfe88
shivam singh <shivamkondersingh@gmail.com>
2021-05-07 12:23:22 +0530

    make it better

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -188,6 +188,13 @@ const PostData= async (e)=>{
                     </div>
                     </div>
             </section>
+            <ToastContainer position = "top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick rtl={false}
+                    pauseOnFocusLoss draggable pauseOnHover />
+
 
 </div>
 
